f3afd6027d09b49b6556c0e95577e3b2c789f346
Dmitry Shirokov <61240384+DmitrySH19@users.noreply.github.com>
2021-03-29 23:57:41 +0300

    add examples

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,7 @@
 
 int main()
 {
-    return 0;
+    faStr1("as2 12 ss34 a1s sadfvzxc4 gdsgcx23 safxx 123 sf3 5 4  76");
+    faStr2("Ass 2s2 SXGV ss X4 SDxFf");
+    faStr3("sadasd agf sdgryt vx tdj fd zx f ewa 121 2edad");
 }
