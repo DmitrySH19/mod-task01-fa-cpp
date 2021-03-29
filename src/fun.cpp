7213df2be6750b4b54c9206c04e1aa4ae8c89c13
Dmitry Shirokov <61240384+DmitrySH19@users.noreply.github.com>
2021-03-30 00:46:08 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,29 +2,34 @@
 #include <fun.h>
 #include <math.h>
 
-unsigned int faStr1(const char *str)
+unsigned int faStr1(const char* str)
 {
     int i = 0;
     int count = 0;
-    int inWord = 0;
-    while(str[i] != '\0') {
-       if(str[i] != ' ' && inWord == 0 && isdigit(str[i]) == 0) {
-           inWord = 1;
-           count++;
-       }
-       else if(str[i] != ' ' && isdigit(str[i]) == 1 && inWord == 0) {
-           inWord = 3;
-       }
-       else if(str[i] != ' ' && isdigit(str[i]) == 1 && inWord == 1) {
-           count--;
-           inWord = 3;
-       }
-       else if(str[i] == ' ' && (inWord == 1 || inWord == 3)) {
-           inWord = 0;
-       }
-       i++;
+    bool inWord = false;
+    bool isNumber = false;
+
+    while (str[i] != '\0'){
+        if (str[i] != ' ' && !inWord){
+            inWord = true;
+
+            if (isdigit(str[i])){
+                isNumber = true;
+            }
+        }
+        else if ((str[i] != ' ') && inWord && isdigit(str[i])){
+            isNumber = true;
+        }
+        else if (str[i] == ' '){
+            if (!isNumber && inWord){
+                count++;
+            }
+            inWord = false;
+            isNumber = false;
+        }
+        i++;
     }
-    return count
+    return count;
 }
 unsigned int faStr2(const char *str)
 {
